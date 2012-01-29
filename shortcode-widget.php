f11f84b338fc519bc0b823b08741b069c5105413
Jon Lund Steffensen <jonlst@gmail.com>
2012-01-29 01:31:22 +0100

    Add shortcode-widget.php which allows for shortcodes in the standard Text widget. Remove Paypal widget as the same thing can be done with the Text widget now.

---
+++ shortcode-widget.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @package Shortcode-Widget
+ * @author Jon Lund Steffensen
+ * @version 0.1
+ */
+/*
+Plugin Name: Shortcode-Widget
+Plugin URI: http://wordpress.org/#
+Description: Use shortcodes in widgets.
+Author: Jon Lund Steffensen
+Version: 0.1
+Author URI: http://jonls.dk/
+*/
+
+if (!is_admin()) add_filter('widget_text', 'do_shortcode', 11);
